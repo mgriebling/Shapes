772a51fcc6f3c4bfc39e17579455e4fcf39e8872
Michael Griebling <sam@c-inspirations.com>
2023-03-22 10:18:49 -0400

    Initial Commit

---
+++ Shapes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shapes
+//
+//  Created by Mike Griebling on 22.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Mike Griebling on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
